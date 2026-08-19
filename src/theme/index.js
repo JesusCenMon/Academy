79869b183383534d3127dbb1ec69f2cace05198f
DowuerWork <dowueruniversidad@gmail.com>
2026-08-19 18:08:27 -0500

    Agregar componentes y datos de clases

---
+++ src/theme/index.js
@@ -0,0 +1,23 @@
+export {Platform} from "react-native";
+
+export const colors = {
+    fondo: '#',
+    primario: '',
+    texto: '',
+    borde: ''
+}
+
+export const spacing = {
+    xs:4,
+    sn: 8,
+    md: 12,
+    lg: 16,
+    xl: 20
+}
+
+export const typography ={
+    titulo: {fontSize: 26, fontWeight: '800', color: colors.texto},
+    subtitulo: {fontSize: 18, fontWeight: '600', color: colors.texto}
+}
+
+export default {colors, spacing, typography}
